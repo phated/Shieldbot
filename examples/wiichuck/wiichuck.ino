48c2e90f00e28de2c7a7f63ebfb04ecf5b71b459
Colin Ho <colinho42@gmail.com>
2013-01-06 19:18:35 -0700

    wiichuck demo added

---
+++ examples/wiichuck/wiichuck.ino
@@ -0,0 +1,49 @@
+/* You need a nintendo wiichuck and you probably want one of these 
+ * from sparkfun to make your life easier 
+ * https://www.sparkfun.com/products/9281
+ *
+ * IMPORTANT NOTE: you MUST turn your A2 and A3 (S3 and S4) switches OFF to use
+ * as the wiichuck adapter uses those for power and ground.
+ */
+
+#include <Shieldbot.h>
+
+#include <Wiichuck.h> 
+#include <Wire.h> //Must be included for wiichuck
+
+Shieldbot shieldbot = Shieldbot();
+Wiichuck wii = Wiichuck();
+
+void setup(){
+  Serial.begin(9600);
+  shieldbot.setMaxSpeed(255);//255 is max
+  wii.init();
+  wii.calibrate();
+}
+
+void loop(){
+  wii.poll();
+  int joy_x_axis = wii.joyX();
+  int joy_y_axis = wii.joyY();
+
+  Serial.print("nunx = ");
+  Serial.println(joy_x_axis);
+  Serial.print("nuny= ");
+  Serial.println(joy_y_axis);
+  
+  int X1 = map(joy_x_axis, 26, 228, 127, -128);
+  int Y = map(joy_y_axis, 24, 228, -128, 127);
+  int lVal = constrain(X1+Y, -128, 127);
+  
+  int X2 = map(joy_x_axis, 26, 228, -128, 127);
+  int rVal = constrain(X2+Y, -128, 127);
+  
+  shieldbot.leftMotor(lVal);
+  shieldbot.rightMotor(rVal);
+  
+  Serial.print("LMval = ");
+  Serial.println(lVal);
+  Serial.print("RMval= ");
+  Serial.println(rVal);
+
+}
